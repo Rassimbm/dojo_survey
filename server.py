4cac3b6436598f4d07a0bf9f8f2e5d24398bc17c
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2024-02-11 16:09:51 -0600

    Have the /result route display the information from the form on a new HTML page 'result.html'

--- server.py
+++ server.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "8891techa0128"
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/process", methods = ["POST"])
+def p_survey():
+    if request.method == "POST":
+        user_name = request.form["user_name"]
+        location = request.form["select_dojo"]
+        language = request.form["select_language"]
+
+        session["user_name"] = user_name
+        session["dojo_location"] = location
+        session["favorite_language"] = language
+        return redirect("/result")
+    
+@app.route("/result")
+def r_survey():
+    if not any(key in session for key in ["user_name", "dojo_location", "favorite_language"]):
+        return redirect("/")
+    user_name = session["user_name"]
+    dojo_location = session["dojo_location"]
+    favorite_language = session["favorite_language"]
+    return render_template("result.html", user_name=user_name, dojo_location = dojo_location, favorite_language = favorite_language)
 
 if __name__ == ("__main__"):
     app.run(debug=True)
